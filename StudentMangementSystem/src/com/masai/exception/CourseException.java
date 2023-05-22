7f669c311dbe698cc7831439690c9281f7f02814
PremKumarAK47 <premkumar147369@gmail.com>
2023-05-22 19:17:30 +0530

    bean

---
+++ StudentMangementSystem/src/com/masai/exception/CourseException.java
@@ -0,0 +1,15 @@
+package com.masai.exception;
+
+public class CourseException extends Exception{
+	
+	public CourseException() {
+		// TODO Auto-generated constructor stub
+	}
+	public CourseException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+	
+
+}
